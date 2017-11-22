220df74e5cb164262b69b2d19f9358e1cdcd75aa
Jinsung Ha <jsung5381@naver.com>
2017-11-22 23:59:16 +0000

    Added more configuration files for refactoring.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,19 @@
 
 # configuration variables for class Event and its inner class(es).
+min_price = 1
+max_price = 50
+
+min_nid = 1
+max_nid = 3
+
+min_nb_tickets = 0
+max_nb_tickets = 10
+
+min_dollars_len = 2
+max_pennies_len = 2
+
 padding_size = 3
+
 
 # configuration variables for class Grid and its inner class(es).
 width = 10
--- src/event.py
+++ src/event.py
@@ -15,7 +15,7 @@ class Event:
         def __init__(self):
             # "Each ticket has a non-zero price, expressed in US Dollars."
             # Assume that each ticket has price between 1 and 50 US Dollars.
-            self.price = float("{0:.2f}".format(random.uniform(1, 50)))
+            self.price = float("{0:.2f}".format(random.uniform(config.min_price, config.max_price)))
 
         def __lt__(self, other):
             return self.price < other.price
@@ -25,21 +25,19 @@ class Event:
 
         def format_price(self, price):
             split = price.split('.')
-            dollars = split[0].zfill(2)
-            pennies = split[1].ljust(2, '0')
+            dollars = split[0].zfill(config.min_dollars_len)
+            pennies = split[1].ljust(config.max_pennies_len, '0')
             return '${}.{}'.format(dollars, pennies)
 
     def __init__(self):
         # "Each event has a unique numeric identifier between 1 and 3."
-        self.nid = random.randint(1, 3)
+        self.nid = random.randint(config.min_nid, config.max_nid)
         
         # "Each event has zero or more tickets."
         # Assume that each event can hold a maximum of 10 tickets.
-        self.tickets = [self.Ticket() for _ in range (random.randint(0, 10))]
+        self.tickets = [self.Ticket() for _ in range (random.randint(config.min_nb_tickets, config.max_nb_tickets))]
         # Assume that ticket list is sorted in terms of their price.
         self.tickets.sort()
-
-        #self.pos = pos
         
     def __str__(self):
         padded_nid = str(self.nid).zfill(config.padding_size)
